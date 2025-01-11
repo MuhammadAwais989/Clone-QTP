9069220550fc891bc5f4993e8d876d9f0abcdd52
MuhammadAwais989 <officialawais989@gmail.com>
2025-01-11 14:44:37 +0500

    working

--- src/Components/Header/License/Learner.jsx
+++ src/Components/Header/License/Learner.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useState } from 'react'
 import LearnerHeader from '../about/Offices/OfficesHeader'
+import New from './SubLearner/New'
 
 function Learner() {
+  const [activebutton,setactivebutton] = useState('New'); 
+
+  const buttonHandle = (btntype) => {
+    setactivebutton(btntype)
+  }
   return (
     <>
     {/* Header */}
@@ -10,7 +17,27 @@ function Learner() {
     {/* Main */}
     <div className="learner-cont">
       <div className="learner-main">
-       
+       <div className="leaner-btns">
+        <button
+        className={activebutton === "New" ? "leaneractive" : ""}
+        onClick={() => buttonHandle("New")}
+        >
+          New
+        </button>
+        <button
+        className={activebutton === "Duplicate" ? "leaneractive" : ""}
+        onClick={() => buttonHandle("Duplicate")}
+        >Duplicate</button>
+        <button
+        className={activebutton === "Endorsement" ? "leaneractive" : ""}
+        onClick={() => buttonHandle("Endorsement")}
+        >Endorsement</button>
+       </div>
+
+       {/* Content Section */}
+       <div className="leaner-content">
+        {activebutton === "New" && <New/>}
+       </div>
       </div>
     </div>
     </>
