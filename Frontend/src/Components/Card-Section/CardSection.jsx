510268ec2fd4eb22d3f3dc03ac5ed1e53283288c
MuhammadAwais989 <officialawais989@gmail.com>
2025-02-05 14:07:45 +0500

    working

--- Frontend/src/Components/Card-Section/CardSection.jsx
+++ Frontend/src/Components/Card-Section/CardSection.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import verify from '../../images/Verify.123cc5f85d298b449ecd.png';
-import learner from '../../images/learnerPermit.a9121a5ce76ceade5226.png';
-import permanent from '../../images/Permanent License.2aa0bb1fcd0b9f3e0ad5.png';
-import international from '../../images/International.d6b774abc970659a1047.png';
-import duplicatee from '../../images/Duplicate 2.d700aa89513028b6be7f.png';
-import Card from './CardChild';
+import React from "react";
+import verify from "../../images/Verify.123cc5f85d298b449ecd.png";
+import learner from "../../images/learnerPermit.a9121a5ce76ceade5226.png";
+import permanent from "../../images/Permanent License.2aa0bb1fcd0b9f3e0ad5.png";
+import international from "../../images/International.d6b774abc970659a1047.png";
+import duplicatee from "../../images/Duplicate 2.d700aa89513028b6be7f.png";
+import managment from "../../images/tafficManagmentcard.png";
+import problem from "../../images/tafficproblemcard.png";
+import challan from "../../images/warning 1.5707d8a18da1290565e1.png";
+import vectortop from "../../images/vector-top.b22e6170ecdd7ec3d712.png";
+import Card from "./CardChild";
 
 function CardSection() {
+  const [trafficCard, settrafficCard] = React.useState(true);
   const cardData = [
     {
       image: verify,
@@ -44,25 +49,81 @@ function CardSection() {
       linkUrl: "",
     },
   ];
+  const trafficCardData = [
+    {
+      image: managment,
+      title: "Traffic Management",
+      description: "Traffic controlled via City and Sariab Circles.",
+      linkText: "Get Info",
+      linkUrl: "",
+    },
+    {
+      image: problem,
+      title: "Traffic Problem",
+      description: "Traffic Police need to address many challenges.",
+      linkText: "Get Info",
+      linkUrl: "",
+    },
+    {
+      image: challan,
+      title: "Challan/Fines",
+      description: "Traffic Police must address challan/fine challenges.",
+      linkText: "Get Info",
+      linkUrl: "",
+    },
+  ];
 
   return (
-    <div 
-      className="card-container"
-      data-aos="fade-up"
-      data-aos-duration="1000"
-    >
-      {cardData.map((card, index) => (
-        
-        <Card
-          key={index}
-          image={card.image}
-          title={card.title}
-          description={card.description}
-          linkText={card.linkText}
-          linkUrl={card.linkUrl}
-        />
-      ))}
-    </div>
+    <>
+      <div className="S-two-cont">
+        <div className="img-div">
+          <img src={vectortop} alt="" />
+        </div>
+        <div className="btn-div">
+          <button className="s-t-btn-f" onClick={() => settrafficCard(true)}>
+            Driving License
+          </button>
+          <button className="s-t-btn" onClick={() => settrafficCard(false)}>
+            Traffic
+          </button>
+        </div>
+      </div>
+      {trafficCard ? (
+        <div
+          className="card-container"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          {cardData.map((card, index) => (
+            <Card
+              key={index}
+              image={card.image}
+              title={card.title}
+              description={card.description}
+              linkText={card.linkText}
+              linkUrl={card.linkUrl}
+            />
+          ))}
+        </div>
+      ) : (
+        <div
+          className="card-container"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          {trafficCardData.map((card, index) => (
+            <Card
+              key={index}
+              image={card.image}
+              title={card.title}
+              description={card.description}
+              linkText={card.linkText}
+              linkUrl={card.linkUrl}
+            />
+          ))}
+        </div>
+      )}
+    </>
   );
 }
 
--- Frontend/src/Page/Home.jsx
+++ Frontend/src/Page/Home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import {HeroSection} from "../Components/Hero/HeroSection";
-import {SectionTwo} from "../Components/Second-Section/Section-two";
 import CardSection from "../Components/Card-Section/CardSection";
 import ThirdSec from "../Components/Third-Section/ThirdSec";
 import PublicMessage from "../Components/Public-Message/PublixMessage";
 import {Service} from "../Components/Service-Section/Service";
 import AppSection from "../Components/App-Section/AppSection";
 import Message from "../Components/Caretaker/CaretakerChild";
-// import Footer from "../Components/Footer/Footer";
 
 
 
@@ -15,14 +13,12 @@ function Home() {
   return (
     <>  
       <HeroSection />
-      <SectionTwo />
       <CardSection />
       <ThirdSec />
       <PublicMessage />
       <Service /> 
       <AppSection/>
       <Message/>
-      {/* <Footer/> */}
     </>
   );
 }
